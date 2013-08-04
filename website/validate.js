819578da1d7523e532a9b2b536a6ad29d34b3f6a
Pushkar Raj <mehandiratta@Manu.(none)>
2013-08-05 01:18:47 +0530

    feedback-form validation improved

--- website/validate.js
+++ website/validate.js
@@ -4,12 +4,14 @@ function formValidator(){
   
   var name = document.getElementById('name');
   var email = document.getElementById('email');
-  
+  var message = document.getElementById('message');
   
   // Check each input in the order that it appears in the form!
-  if(isAlphabet(name, "Please enter only letters for Name")){
-     if(emailValidator(email, "Please enter a valid email address")){
+  if(isAlphabet(name, "Please enter only Alphabets for Name")){
+     if(emailValidator(email, "Please enter a valid Email Address")){
+	if(notempty(message, "The message field can not be left blank")){
         return true;
+		    }
                   }
                 }
   return false;
@@ -22,7 +24,12 @@ function formValidator(){
         
 function isAlphabet(elem, helperMsg){
   var alphaExp = /^[a-zA-Z][a-zA-Z ]+$/;
-  if(elem.value.match(alphaExp)){
+  var len = elem.value.length;
+    if(len==0){
+	alert("The Name field can not be left blank");
+	elem.focus();
+        return false;
+	} else if(elem.value.match(alphaExp)){
     return true;
   }else{
     alert(helperMsg);
@@ -35,11 +42,27 @@ function isAlphabet(elem, helperMsg){
 
 function emailValidator(elem, helperMsg){
   var emailExp = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-  if(elem.value.match(emailExp)){
+  var len = elem.value.length;
+    if(len==0){
+	alert("The Email field can not be left blank");
+	elem.focus();
+        return false;
+	} else if(elem.value.match(emailExp)){
     return true;
-  }else{
+  } else {
     alert(helperMsg);
     elem.focus();
     return false;
   }
+}
+
+function notempty(elem, helperMsg){
+    var len = elem.value.length;
+    if(len==0){
+	alert(helperMsg);
+	elem.focus;
+	return false;
+	} else {
+	    return true;
+	    }
 }
